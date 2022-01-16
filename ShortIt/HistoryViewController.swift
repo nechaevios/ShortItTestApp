dcf6f516e2cec3cb8d6c2853f957c22ef7b01381
Sergey <21218903+xchakiex@users.noreply.github.com>
2022-01-16 14:39:00 +0300

    init app delegate setup tabBar + navBar + mainVC + HistoryVC prepared

---
+++ ShortIt/HistoryViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HistoryViewController.swift
+//  ShortIt
+//
+//  Created by Nechaev Sergey  on 15.01.2022.
+//
+
+import UIKit
+
+class HistoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+        navigationItem.title = "History"
+    }
+    
+
+}
