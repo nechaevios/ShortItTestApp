55e5a8546bb7f8c5d301940d30037f0c8d770d79
Sergey <21218903+xchakiex@users.noreply.github.com>
2022-01-17 16:09:43 +0300

    historyVC prepared

--- ShortIt/ViewControllers/HistoryViewController.swift
+++ ShortIt/ViewControllers/HistoryViewController.swift
@@ -8,12 +8,90 @@
 import UIKit
 
 class HistoryViewController: UIViewController {
-
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .white
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    
+    private var responses: [ResponseModel]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
+        
+        responses = ResponseModel.getResponses()
+        
+        setupViews()
+        setupDelegate()
+        
+        setupUI()
+        setupConstraints()
+    }
+    
+    private func setupViews() {
+        view.addSubview(tableView)
+    }
+    
+    private func setupDelegate() {
+//        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    private func setupUI() {
+        view.backgroundColor = .white
         navigationItem.title = "History"
     }
     
-
+    private func setupConstraints() {
+        setupTableViewConstraints()
+    }
+    
+    private func setupTableViewConstraints() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
+        tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+    }
+    
 }
+
+//MARK: - UITableViewDataSource
+
+extension HistoryViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        responses.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let response = responses[indexPath.row]
+        if #available(iOS 14.0, *) {
+            var content = cell.defaultContentConfiguration()
+            content.text = response.longurl
+            content.secondaryText = response.shorturl
+            cell.contentConfiguration = content
+        } else {
+            cell.textLabel!.text = response.shorturl
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            responses.remove(at: indexPath.row)
+            tableView.reloadData()
+        }
+    }
+    
+    
+}
+
+//MARK: - UITableViewDelegate
+
+//extension HistoryViewController: UITableViewDelegate {
+//
+//}
