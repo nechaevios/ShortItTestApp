0a9a47a77a26b4b068fd9b5bc4d6f4d2c3dbc044
Sergey <21218903+xchakiex@users.noreply.github.com>
2022-01-25 14:15:42 +0300

    HistoryVC initial commit

---
+++ ShortIt/HistoryView/HistoryViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  HistoryViewModel.swift
+//  ShortIt
+//
+//  Created by Nechaev Sergey  on 21.01.2022.
+//
+
+import Foundation
+
+protocol HistoryViewModelProtocol {
+    
+}
+
+class HistoryViewModel: HistoryViewModelProtocol {
+    
+}
