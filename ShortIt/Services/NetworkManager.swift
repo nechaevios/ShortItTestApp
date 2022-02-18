cf6ddf6cff3698208d154da456a68c411ee61902
Sergey <21218903+xchakiex@users.noreply.github.com>
2022-02-18 16:29:41 +0300

    update NetworkManager

--- ShortIt/Services/NetworkManager.swift
+++ ShortIt/Services/NetworkManager.swift
@@ -7,20 +7,32 @@
 
 import Foundation
 
-final class NetworkManager {
+class NetworkManager {
     
     static let shared = NetworkManager()
     
+    private enum Domains: String {
+        case bitly = "bit.ly"
+    }
+    
+    private enum ApiUrls: String {
+        case shortenApiUrl = "https://api-ssl.bitly.com/v4/shorten"
+    }
+    
+    private enum ApiKeys: String {
+        case bitLyApiKey = "076df6aa095ff080918c787e69ee52a5b6854e94"
+    }
+        
     private init() {}
     
-    func fetchShortUrl(longUrl: String, completion: @escaping (Response?, Error?) -> Void) {
+    func fetchShortUrl(longUrl: String, completion: @escaping (BitLyResponse?, Error?) -> Void) {
         fetchData(urlString: longUrl) { result in
             switch result {
                 
             case .success(let data):
                 do {
-                let response = try JSONDecoder().decode(Response.self, from: data)
-                completion(response, nil)
+                    let response = try JSONDecoder().decode(BitLyResponse.self, from: data)
+                    completion(response, nil)
                 } catch let jsonError {
                     print(jsonError)
                 }
@@ -33,11 +45,23 @@ final class NetworkManager {
     
     private func fetchData(urlString: String, completion: @escaping (Result<Data, Error>) -> Void) {
         
-        let apiUrl = "http://tiny-url.info/api/v1/random?format=json&url="
+        struct PostData: Codable {
+            let long_url: String
+            var domain = Domains.bitly.rawValue
+        }
         
-        guard let url = URL(string: apiUrl+urlString) else { return }
+        let postData = PostData(long_url: urlString)
+        guard let jsonData = try? JSONEncoder().encode(postData) else { return }
+        guard let url = URL(string: ApiUrls.shortenApiUrl.rawValue) else { return }
         
-        URLSession.shared.dataTask(with: url) { data, _, error in
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("application/json", forHTTPHeaderField: "Accept")
+        request.setValue(ApiKeys.bitLyApiKey.rawValue, forHTTPHeaderField: "Authorization")
+        request.httpBody = jsonData
+        
+        URLSession.shared.dataTask(with: request) { data, _, error in
             DispatchQueue.main.async {
                 if let error = error {
                     completion(.failure(error))
